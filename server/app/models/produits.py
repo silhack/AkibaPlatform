8deede8a6b8f9a94a79a7bcfea5e1c95aedcd439
Z-Isaac08 <ezechielncho225@gmail.com>
2025-04-30 21:27:58 +0000

    Update: "Mise à jour du modèle produits "

--- server/app/models/produits.py
+++ server/app/models/produits.py
@@ -1,24 +1,33 @@
-from sqlmodel import JSON, Column, SQLModel, Field
+from sqlmodel import Relationship, SQLModel, Field
 import uuid
 from typing import Optional, List
 
 # PRODUITS MODELES
+class ProduitAvantageLink(SQLModel, table=True):
+  produit_id: uuid.UUID | None = Field(default=None, foreign_key="produits.id", primary_key=True)
+  avantage_id: int | None = Field(default=None, foreign_key="avantage.id", primary_key=True)
 
 class ProduitsBase(SQLModel):
   image: str | None = None
   nom: str = Field(index=True)
   accroche: str
   description: str
-  avantages: Optional[List[str]] = Field(sa_column=Column(JSON))
+
+class Avantage(SQLModel, table=True):
+  id: Optional[int] = Field(default=None, primary_key=True)
+  nom: str = Field(index=True, unique=True)
+  produits: List["Produits"] = Relationship(back_populates="avantages", link_model=ProduitAvantageLink)
 
 class Produits(ProduitsBase, table=True):
   id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+  avantages: List[Avantage] = Relationship(back_populates="produits", link_model=ProduitAvantageLink) 
 
 class ProduitsCreate(ProduitsBase):
-  pass
+  avantages: List[str]
 
 class ProduitsPublic(ProduitsBase):
   id: uuid.UUID
+  avantages: List[str]
 
 class ProduitsUpdate(SQLModel):
   image: str | None = None
